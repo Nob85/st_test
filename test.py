0915bf16306c017e57ed6a35b9001e7e84764697
市場開発 津田登 <tudan@cats.tosho.co.jp>
2024-09-16 23:13:16 +0900

    Initialize repository

---
+++ test.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import MeCab
+import random
+import time
+
+# st.title("今日もきっといい日")
+st.title("ポケットやる気メーター")
+st.text("テキストボックスに今日心に残った言葉を記入してください")
+
+text_input = st.text_area("心に残った一言を記入してください", "")
+mecab = MeCab.Tagger()
+tmp = mecab.parse(text_input)
+rel = tmp.split("\n")
+s_list = [s.split(",")[0].split("\t") for s in rel[:-2]]
+noun = []#noun
+adje = []#adjective
+inte = []#interjection
+other = []
+for s_word in s_list:
+    if s_word[1] == "名詞":
+        noun.append(s_word[0])
+    elif s_word[1] == "形容詞":
+        adje.append(s_word[0])
+    elif s_word[1] == "感動詞":
+        inte.append(s_word[0])
+    else:
+        other.append(s_word[0])
+
+a = len(inte)*random.randint(75, 100)
+b = len(adje)*random.randint(50, 75)
+c = len(noun)*random.randint(25, 50)
+d = len(other)*random.randint(0, 25)
+ans = a+b+c+d
+
+now = 2556
+if st.button("Check"):
+    with st.spinner("計測中"):
+        time.sleep(3)
+    st.write(f"あなたは今日、{ans} ポイント感謝されました🎉")
+    now += ans
+    st.write("👏👏👏","\n\n")
+st.write(f"今日までの累積ポイントは　{now} ポイント　です😃")
+
+
+
